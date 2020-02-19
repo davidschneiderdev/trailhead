2d8807be3d36d59b251ea4eb84c4552374f2a69f
David Schneider <david.jay.schneider@gmail.com>
2020-02-18 23:05:54 -0500

    adds updates to SearchApp functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import SearchApp from './components/SearchApp';
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
         <SearchApp />
-    </div>
+    </AppContainer>
   );
 }
 
--- src/components/SearchApp.js
+++ src/components/SearchApp.js
@@ -10,7 +10,6 @@ const atlantaAreas = [
     { label: 'Norcross', value: 3 },
     { label: 'Roswell', value: 4 },
     { label: 'Rivermont', value: 5 },
-
 ]
 
 const SearchAppContainer = styled.div`
@@ -23,14 +22,13 @@ const SearchAppContainer = styled.div`
 `;
 
 const StyledOptions = styled.form`
-    margin: 2em 5em 2em 0em;
+    width: 100%;
     text-align: left;
 `;
 
-const StyledSortBy = styled(Form.Group)`
-    
+const StyledSortBySection = styled(Form.Group)`
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
     color: gray;
 `;
@@ -40,12 +38,19 @@ const StyledSortByTitle = styled(Form.Label)`
     font-size: 16px;
 `;
 
+const MaxMinSection = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1em;
+`;
+
 const StyledSortRadioList = styled(Row)`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: flex-start;
     align-items: flex-start;
+    margin-left: 1em;
     color: gray;
 `;
 
@@ -54,6 +59,7 @@ const StyledRadioButton = styled(Form.Check)`
 `;
 
 const StyledButton = styled(Button)`
+    width: 100%;
     background-color: gray;
 
 `;
@@ -64,16 +70,40 @@ export default class SearchApp extends React.Component {
         super(props);
         this.state = {
             selectedOption: null,
-            maxDistance: 100,
+            maxDistance: 0,
             minDistance: 0,
-            sortBy: 'distance',
-            numStars: 2
+            sortBy: 'rating',
+            minNumStars: 2
         }
     }
 
-    _handleChange = (selectedOption) => {
+    _updateSelectedOption = (selectedOption) => {
         console.log(selectedOption.label);
         this.setState({ selectedOption })
+    }
+
+    _updateMaxDist = (event) => {
+        this.setState({
+            maxDistance: event.target.value
+        })
+    }
+
+    _updateMinDist = (event) => {
+        this.setState({
+            minDistance: event.target.value
+        })
+    }
+
+    _updateSortBy = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            sortBy: event.target.value
+        });
+    }
+
+    _printState = (event) => {
+        event.preventDefault();
+        console.table(this.state);
     }
 
     render() {
@@ -82,17 +112,25 @@ export default class SearchApp extends React.Component {
                 <Select options= { atlantaAreas } 
                             isSearchable
                             placeholder='Select Atlanta Region'
-                            onChange={this._handleChange} />
+                            onChange={this._updateSelectedOption} />
                 <StyledOptions>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="integer" placeholder="Maximum Distance - Miles" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Control type="integer" placeholder="Minimum Distance - Miles" />
-                    </Form.Group>
-                    <StyledSortBy>
+                    <MaxMinSection>
+                        <Form.Group>
+                            <Form.Control 
+                                type="integer" 
+                                placeholder="Max Distance"
+                                onChange={this._updateMaxDist} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Control 
+                                type="integer" 
+                                placeholder="Min Distance"
+                                onChange={this._updateMinDist} />
+                        </Form.Group>
+                    </MaxMinSection>
+                    <StyledSortBySection>
                         <StyledSortByTitle >
-                            Sort By:
+                            Sort Results By:
                         </StyledSortByTitle>
                         <StyledSortRadioList>
                             <StyledRadioButton
@@ -100,16 +138,23 @@ export default class SearchApp extends React.Component {
                             label="Distance"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios1"
+                            value="distance"
+                            onChange={this._updateSortBy}
                             />
                             <StyledRadioButton
                             type="radio"
                             label="Rating"
                             name="formHorizontalRadios"
                             id="formHorizontalRadios2"
+                            value="rating"
+                            onChange={this._updateSortBy}
                             />
                         </StyledSortRadioList>
-                    </StyledSortBy>
-                    <StyledButton variant="primary" type="submit">
+                    </StyledSortBySection>
+                    <StyledButton 
+                        variant="primary" 
+                        type="submit"
+                        onClick={this._printState}>
                         Search Trails
                     </StyledButton>
                 </StyledOptions>
